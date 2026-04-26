f26cc7ffb74ae5515c672a7b0471c5a004767a50
NVinox <vomilk24@gmail.com>
2026-04-26 21:59:47 +0700

    feat: добавил глобальный интерцептор для ответов и фильтр ошибки

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { AllExceptionFilter } from './filters/allException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionFilter());
 
   await app.listen(process.env.APPLICATION_PORT ?? 3000);
 }
