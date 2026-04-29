f971cf9002f40c85da91778860009b4131ac7280
NVinox <vomilk24@gmail.com>
2026-04-29 22:54:48 +0700

    fix: исправил DTO для правильного отображения в swagger

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,62 +1,8 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import {
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-  IsString,
-  Length,
-  Max,
-  MaxLength,
-  Min,
-} from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+import { CreatePatchProductDTO } from './create-patch-product.dto';
 
-export class CreateProductDTO {
-  @ApiProperty({
-    description: 'Название продукта',
-    type: 'string',
-    example: 'Молоко',
-  })
-  @IsString()
-  @IsNotEmpty()
-  @Length(3, 255)
-  title!: string;
-
-  @ApiProperty({
-    description: 'Актуальная цена',
-    type: 'number',
-    example: 100,
-  })
-  @IsNumber()
-  @IsNotEmpty()
-  @Type(() => Number)
-  @IsPositive()
-  price!: number;
-
-  @ApiProperty({
-    description: 'Артикул (уникальное значение)',
-    type: 'string',
-    example: '000001',
-  })
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(255)
-  sku!: string;
-
-  @ApiPropertyOptional({
-    description: 'Скидка на продукт',
-    type: 'integer',
-    example: 10,
-  })
-  @IsInt()
-  @IsOptional()
-  @Type(() => Number)
-  @Max(100)
-  @Min(0)
-  discount!: number;
-
+export class CreateProductDTO extends CreatePatchProductDTO {
   @ApiPropertyOptional({
     description: 'Массив изображений',
     type: 'array',
--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,4 @@
-import { CreateProductDTO } from './create-product.dto';
 import { PartialType } from '@nestjs/swagger';
+import { CreatePatchProductDTO } from './create-patch-product.dto';
 
-export class UpdateProductDTO extends PartialType(CreateProductDTO) {}
+export class UpdateProductDTO extends PartialType(CreatePatchProductDTO) {}
