44f12738f9c24b3c152eaaad57b64298e253c8fd
NVinox <vomilk24@gmail.com>
2026-04-29 22:49:50 +0700

    fix: исправил DTO обновления продукта

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,4 @@
 import { CreateProductDTO } from './create-product.dto';
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 
 export class UpdateProductDTO extends PartialType(CreateProductDTO) {}
