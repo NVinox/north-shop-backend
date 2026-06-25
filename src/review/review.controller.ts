1a7738205e994e43424229d0d20c97c3b771221c
NVinox <vomilk24@gmail.com>
2026-06-25 20:48:05 +0700

    feat: релизовал пагинацию при запросе отзывов продукта

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 
 import { ReviewService } from './review.service';
 
 import { ResponseReviewDTO } from './dto/response-review.dto';
+import { PaginationResponseDTO } from 'src/common/dto/pagination-response.dto';
+import { PaginationQueryDTO } from 'src/common/dto/pagination-query.dto';
 
 @Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Get()
-  async getAll() {
-    const reviews = await this.reviewService.getAll();
-    return plainToInstance(ResponseReviewDTO, reviews, {
-      excludeExtraneousValues: true,
-    });
+  @Get(':productId')
+  async getAllByProduct(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Query() query: PaginationQueryDTO,
+  ): Promise<PaginationResponseDTO<ResponseReviewDTO>> {
+    const result = await this.reviewService.getAllByProduct(productId, query);
+
+    return {
+      items: plainToInstance(ResponseReviewDTO, result.items, {
+        excludeExtraneousValues: true,
+      }),
+      meta: result.meta,
+    };
   }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { ReviewEntity } from './entities/review.entity';
 
 import { PaginationResponseDTO } from 'src/common/dto/pagination-response.dto';
+import { PaginationQueryDTO } from 'src/common/dto/pagination-query.dto';
 
 @Injectable()
 export class ReviewService {
@@ -13,9 +14,34 @@ export class ReviewService {
     private readonly reviewRepository: Repository<ReviewEntity>,
   ) {}
 
-  async getAll(): Promise<ReviewEntity[]> {
-    const reviews = await this.reviewRepository.find();
+  async getAllByProduct(
+    productId: number,
+    query: PaginationQueryDTO,
+  ): Promise<PaginationResponseDTO<ReviewEntity>> {
+    const queryBuilder = this.reviewRepository.createQueryBuilder('reviews');
 
-    return reviews;
+    queryBuilder.leftJoinAndSelect('reviews.product', 'product');
+    queryBuilder.leftJoinAndSelect('reviews.user', 'user');
+    queryBuilder.where('reviews.product_id = :productId', { productId });
+
+    const { page, limit, sortOrder } = query;
+
+    queryBuilder
+      .orderBy('reviews.createdAt', sortOrder)
+      .offset((page - 1) * limit)
+      .limit(limit);
+
+    const [items, total] = await queryBuilder.getManyAndCount();
+
+    return {
+      items,
+      meta: {
+        totalItems: total,
+        itemCount: items.length,
+        itemsPerPage: limit,
+        totalPages: Math.ceil(total / limit),
+        currentPage: page,
+      },
+    };
   }
 }
