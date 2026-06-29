4ec07c95b48e56461e4a3f26c742b169bed0f768
NVinox <vomilk24@gmail.com>
2026-06-29 23:26:24 +0700

    feat: добавил sragger документацию эндпоинта

--- src/review/dto/response-review.dto.ts
+++ src/review/dto/response-review.dto.ts
@@ -1,19 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Transform } from 'class-transformer';
 
 export class ResponseReviewDTO {
+  @ApiProperty({
+    description: 'ID Отзыва',
+    type: 'integer',
+    example: 1,
+  })
   @Expose()
   id!: number;
 
+  @ApiProperty({
+    description: 'Текст отзыва',
+    type: 'string',
+    example: 'Тестовый текст отзыва',
+  })
   @Expose()
   text!: string;
 
+  @ApiProperty({
+    description: 'Рейтинг',
+    type: 'integer',
+    example: 5,
+  })
   @Expose()
   rating!: number;
 
-  @Expose()
-  createdAt!: Date;
-
+  @ApiProperty({
+    description: 'Имя пользователя',
+    type: 'string',
+    example: 'Иван',
+  })
   @Expose({ name: 'userName' })
   @Transform(({ obj }) => obj.user?.name || 'Аноним')
   userName!: string;
+
+  @ApiProperty({
+    description: 'Дата создания отзыва',
+    type: Date,
+    example: '2026-06-29T15:42:08.851Z',
+  })
+  @Expose()
+  createdAt!: Date;
 }
