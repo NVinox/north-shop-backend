04c81b4bc2b95a8925eb52c1fe6d405ec2405e95
NVinox <vomilk24@gmail.com>
2026-07-21 05:32:58 +0700

    feat: описал swagger

--- src/productImage/product-image.controller.ts
+++ src/productImage/product-image.controller.ts
@@ -13,6 +13,17 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { FileInterceptor } from '@nestjs/platform-express';
 
@@ -27,12 +38,31 @@ import { Roles } from 'src/common/decorators/roles-decorator';
 import { EUserRole } from 'src/common/enums/user-role.enum';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
+import { ErrorResponseDTO } from 'src/common/dto/error-response.dto';
 
+@ApiTags('Изображения подукта')
 @Controller('product-image')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class ProductImageController {
   constructor(private readonly productImageService: ProductImageService) {}
 
+  @ApiOperation({
+    summary: 'Создание изображения продукта',
+    description: 'Метод создания изображения продукта',
+  })
+  @ApiCreatedResponse({
+    description: 'Изображение создано',
+    type: ResponseProductImageDTO,
+  })
+  @ApiBadRequestResponse({
+    description: 'Ошибка неправильного запроса',
+    type: ErrorResponseDTO,
+  })
+  @ApiForbiddenResponse({
+    description: 'Недостаточно прав',
+    type: ErrorResponseDTO,
+  })
+  @ApiBearerAuth('access-token')
   @UseInterceptors(FileInterceptor('image'))
   @Roles(EUserRole.MANAGER, EUserRole.ADMIN)
   @Post()
@@ -56,6 +86,32 @@ export class ProductImageController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Обновление изображения продукта',
+    description: 'Метод обновления изображения продукта по id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'integer',
+    description: 'ID изображения продукта',
+  })
+  @ApiOkResponse({
+    description: 'Изображение обновлено',
+    type: ResponseProductImageDTO,
+  })
+  @ApiBadRequestResponse({
+    description: 'Ошибка неправильного запроса',
+    type: ErrorResponseDTO,
+  })
+  @ApiNotFoundResponse({
+    description: 'Изображение не найдено',
+    type: ErrorResponseDTO,
+  })
+  @ApiForbiddenResponse({
+    description: 'Недостаточно прав',
+    type: ErrorResponseDTO,
+  })
+  @ApiBearerAuth('access-token')
   @Roles(EUserRole.MANAGER, EUserRole.ADMIN)
   @Patch(':id')
   async update(
@@ -69,6 +125,32 @@ export class ProductImageController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Удаление изображения продукта',
+    description: 'Метод удаления изображения продукта по id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'integer',
+    description: 'ID изображения продукта',
+  })
+  @ApiOkResponse({
+    description: 'Изображение удалено',
+    type: ResponseProductImageDTO,
+  })
+  @ApiBadRequestResponse({
+    description: 'Ошибка неправильного запроса',
+    type: ErrorResponseDTO,
+  })
+  @ApiNotFoundResponse({
+    description: 'Изображение не найдено',
+    type: ErrorResponseDTO,
+  })
+  @ApiForbiddenResponse({
+    description: 'Недостаточно прав',
+    type: ErrorResponseDTO,
+  })
+  @ApiBearerAuth('access-token')
   @Roles(EUserRole.MANAGER, EUserRole.ADMIN)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number): Promise<Boolean> {
